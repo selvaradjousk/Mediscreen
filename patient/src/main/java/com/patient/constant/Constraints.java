4abde4a18f3e79ed9a5236643ffac25fe7fb3e34
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-12-28 17:12:12 +0100

    [FEAT + UNITTESTS] PATIENT-MS: Patient DTO, unit tests & validators implementation => TESTS PASSED

---
+++ patient/src/main/java/com/patient/constant/Constraints.java
@@ -0,0 +1,22 @@
+package com.patient.constant;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class Constraints {
+
+	public static final int FIRST_NAME_MAX_SIZE = 100;
+
+
+    public static final int LAST_NAME_MAX_SIZE = 100;
+
+
+    public static final int SEX_MAX_SIZE = 1;
+
+
+    public static final int ADDRESS_MAX_SIZE = 100;
+
+
+    public static final int PHONE_MAX_SIZE = 15;
+
+}
---
+++ patient/src/test/java/com/patient/unittest/dto/PatientDtoTest.java
@@ -0,0 +1,78 @@
+package com.patient.unittest.dto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.patient.dto.PatientDTO;
+
+@DisplayName("UNIT TESTS - DTO - PATIENT")
+class PatientDtoTest {
+
+	private PatientDTO patientDto;
+	private LocalDate birthday = LocalDate.now();
+
+	@BeforeEach
+	void init_test() {
+		patientDto = new PatientDTO("lastname", "firstname", birthday, "sex", "address", "phone");
+	}
+
+
+	// *******************************************************************	
+		
+	
+	@DisplayName("Get DTO values - "
+			+ "GIVEN DTO value "
+			+ "WHEN Requested DTO value"
+			+ "THEN returns expected DTO values")
+	@Test
+	void testGetPatientValues() {
+		assertEquals("lastname", patientDto.getLastName());
+		assertEquals("firstname", patientDto.getFirstName());
+		assertEquals("sex", patientDto.getSex());
+		assertEquals(birthday, patientDto.getBirthDate());
+		assertEquals("address", patientDto.getAddress());
+		assertEquals("phone", patientDto.getPhoneNumber());
+
+	}
+
+
+	// *******************************************************************	
+		
+	
+	@DisplayName("Get DTO values after SET new values- "
+			+ "GIVEN new DTO value set "
+			+ "WHEN Requested DTO value"
+			+ "THEN returns expected DTO values")
+	@Test
+	void testSetPatientValues() {
+
+		assertEquals("lastname", patientDto.getLastName());
+		assertEquals("firstname", patientDto.getFirstName());
+		assertEquals("sex", patientDto.getSex());
+		assertEquals(birthday, patientDto.getBirthDate());
+		assertEquals("address", patientDto.getAddress());
+		assertEquals("phone", patientDto.getPhoneNumber());
+
+		LocalDate newBirthday = LocalDate.now();
+
+		patientDto.setLastName("newLastname");
+		patientDto.setFirstName("newFirstname");
+		patientDto.setSex("newSex");
+		patientDto.setBirthDate(newBirthday);
+		patientDto.setAddress("newAddress");
+		patientDto.setPhoneNumber("newPhone");
+
+		assertEquals("newLastname", patientDto.getLastName());
+		assertEquals("newFirstname", patientDto.getFirstName());
+		assertEquals("newSex", patientDto.getSex());
+		assertEquals(newBirthday, patientDto.getBirthDate());
+		assertEquals("newAddress", patientDto.getAddress());
+		assertEquals("newPhone", patientDto.getPhoneNumber());
+	}
+
+}
