cddb14fca8d87be6cffa76c04eca8097c424d28d
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-12-29 15:56:16 +0100

    [UNIT TESTS] [SPRINT-1] UI-MS - PUT updatePatient => RUN FAILED

--- ui/src/test/java/com/ui/unittest/controller/HomeControllerTest.java
+++ ui/src/test/java/com/ui/unittest/controller/HomeControllerTest.java
@@ -1,12 +1,7 @@
 package com.ui.unittest.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.time.LocalDate;
-import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -14,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,59 +17,43 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.ui.controller.PatientController;
-import com.ui.dto.PatientDTO;
-import com.ui.proxy.MicroservicePatientProxy;
+import com.ui.controller.HomeController;
 
-@DisplayName("UNIT TESTS - Controller - Patient")
+@DisplayName("UNIT TESTS - Controller - Home")
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(PatientController.class)
+@WebMvcTest(HomeController.class)
 class HomeControllerTest {
 
-    @MockBean
-    private MicroservicePatientProxy patientProxy;
-
+    private final static String HOME_URL = "/";
+    
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private WebApplicationContext context;
 
-    private PatientDTO patientDTO;
-    
-
-    private final static String PATIENT_LIST_URL = "/patient/list/";
-    
-
     @BeforeEach
     public void setup() {
-    	patientDTO = new PatientDTO(1, "Rees", "Pippa",
-                LocalDate.of(1952, 11, 11), "F", "745 West Valley Farms Drive", "628-423-0993");
 
         mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
     }
     
 
     @Test
-    @DisplayName("test patient list - "
-    		+ "Given a patient list,"
-    		+ " when getAllPatient,"
+    @DisplayName("test Home Page - "
+    		+ "Given home page request,"
+    		+ " when getHomePage,"
     		+ " then return Ok status")
-    public void testPatientList() throws Exception {
-        PatientDTO patient2 = new PatientDTO(1, "Rees", "Pippa",
-                LocalDate.of(1952, 11, 11), "F", "745 West Valley Farms Drive", "628-423-0993");
+    public void testGetHomePage() throws Exception {
 
-        when(patientProxy.getPatientList()).thenReturn(Arrays.asList(patientDTO, patient2));
-
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(PATIENT_LIST_URL)
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(HOME_URL)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andReturn();
 
         String content = result.getResponse().getContentAsString();
 
-        verify(patientProxy).getPatientList();
-        assertThat(content).contains("Pippa", "Rees");
+        assertThat(content).contains("Mediscreen");
     }
 
 
--- ui/src/test/java/com/ui/unittest/controller/PatientControllerListPatientsTest.java
+++ ui/src/test/java/com/ui/unittest/controller/PatientControllerListPatientsTest.java
@@ -1,7 +1,12 @@
 package com.ui.unittest.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDate;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,43 +23,59 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.ui.controller.HomeController;
+import com.ui.controller.PatientController;
+import com.ui.dto.PatientDTO;
+import com.ui.proxy.MicroservicePatientProxy;
 
-@DisplayName("UNIT TESTS - Controller - Home")
+@DisplayName("UNIT TESTS - Controller - Patient GetList")
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(HomeController.class)
-class PatientControllerTest {
+@WebMvcTest(PatientController.class)
+class PatientControllerListPatientsTest {
 
-    private final static String HOME_URL = "/";
-    
+    @MockBean
+    private MicroservicePatientProxy patientProxy;
+
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private WebApplicationContext context;
 
+    private PatientDTO patientDTO;
+    
+
+    private final static String PATIENT_LIST_URL = "/patient/list/";
+    
+
     @BeforeEach
     public void setup() {
+    	patientDTO = new PatientDTO(1, "Rees", "Pippa",
+                LocalDate.of(1952, 11, 11), "F", "745 West Valley Farms Drive", "628-423-0993");
 
         mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
     }
     
 
     @Test
-    @DisplayName("test Home Page - "
-    		+ "Given home page request,"
-    		+ " when getHomePage,"
+    @DisplayName("test patient list - "
+    		+ "Given a patient list,"
+    		+ " when getAllPatient,"
     		+ " then return Ok status")
-    public void testGetHomePage() throws Exception {
+    public void testPatientList() throws Exception {
+        PatientDTO patient2 = new PatientDTO(1, "Rees", "Pippa",
+                LocalDate.of(1952, 11, 11), "F", "745 West Valley Farms Drive", "628-423-0993");
 
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(HOME_URL)
+        when(patientProxy.getPatientList()).thenReturn(Arrays.asList(patientDTO, patient2));
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(PATIENT_LIST_URL)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andReturn();
 
         String content = result.getResponse().getContentAsString();
 
-        assertThat(content).contains("Mediscreen");
+        verify(patientProxy).getPatientList();
+        assertThat(content).contains("Pippa", "Rees");
     }
 
 
