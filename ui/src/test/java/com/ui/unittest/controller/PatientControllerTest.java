810678f13568c8abc36feee6bc631220ec45bd80
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-12-29 08:54:29 +0100

    [TESTS][SPRINT-1] UI-MS - Controllers unit tests => TEST RUN => PASSED

---
+++ ui/src/test/java/com/ui/unittest/controller/PatientControllerTest.java
@@ -0,0 +1,60 @@
+package com.ui.unittest.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.ui.controller.HomeController;
+
+@DisplayName("UNIT TESTS - Controller - Home")
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(HomeController.class)
+class PatientControllerTest {
+
+    private final static String HOME_URL = "/";
+    
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @BeforeEach
+    public void setup() {
+
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+    
+
+    @Test
+    @DisplayName("test Home Page - "
+    		+ "Given home page request,"
+    		+ " when getHomePage,"
+    		+ " then return Ok status")
+    public void testGetHomePage() throws Exception {
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(HOME_URL)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+
+        assertThat(content).contains("Mediscreen");
+    }
+
+
+}
