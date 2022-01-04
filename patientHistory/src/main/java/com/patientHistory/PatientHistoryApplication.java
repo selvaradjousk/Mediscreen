10384a92684d2d09f2f23132573cd926bd8ffb48
selvaradjousk <senthil.selvaradjou@gmail.com>
2022-01-04 19:48:38 +0100

    [FEAT] [SPRINT-2] patientHistory-MS - SpringStarterProject => INITIALIZED

---
+++ patientHistory/src/main/java/com/patientHistory/PatientHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.patientHistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientHistoryApplication.class, args);
+	}
+
+}
---
+++ patientHistory/src/test/java/com/patientHistory/PatientHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patientHistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
