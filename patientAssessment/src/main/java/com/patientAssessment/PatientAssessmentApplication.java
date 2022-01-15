7c7b71939a00885c27a6fd07779a11821a4c6bce
selvaradjousk <senthil.selvaradjou@gmail.com>
2022-01-15 15:10:47 +0100

    [SPRINT 3] PatientAssessment MICROSERVICE - Spring Boot Project initialization

---
+++ patientAssessment/src/main/java/com/patientAssessment/PatientAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.patientAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientAssessmentApplication.class, args);
+	}
+
+}
---
+++ patientAssessment/src/test/java/com/patientAssessment/PatientAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patientAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
