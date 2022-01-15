7733fc7e67c608e85da5072a6bb92c796ab54113
selvaradjousk <senthil.selvaradjou@gmail.com>
2022-01-15 23:32:42 +0100

    [FEAT] [SPRINT-3] ui-MS - AssessmentDTO > IMPLMENTATED

---
+++ ui/src/main/java/com/ui/dto/AssessmentDTO.java
@@ -0,0 +1,17 @@
+package com.ui.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AssessmentDTO {
+
+    private PatientDTO patientDTO;
+
+    private String diabetesRiskLevel;
+}
